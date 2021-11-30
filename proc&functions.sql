53bbf76aec290ffee32c6daee25828da4fc86b55
PrashantSBasnet <50170332+PrashantSBasnet@users.noreply.github.com>
2021-11-30 11:35:12 +0545

    Create proc&functions.sql

---
+++ proc&functions.sql
@@ -0,0 +1,54 @@
+
+/*function with no arguments*/
+CREATE OR REPLACE FUNCTION testfunction()
+    RETURNS TABLE ("id" integer, "name" varchar, "details" varchar)
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    RETURN QUERY
+        SELECT cs.id, cs.new_address, cs.new_company_name FROM company_shareholder cs;
+end;
+$$;
+
+alter function testfunction() owner to postgres; --automatically generated  --- postgres db can only access
+
+
+select id from testfunction()
+
+
+
+/* function with arguments */
+CREATE OR REPLACE FUNCTION testfunction2(idd integer)
+    RETURNS TABLE ("id" integer, "name" varchar, "details" varchar)
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    RETURN QUERY
+        SELECT cs.id, cs.new_address, cs.new_company_name
+        FROM company_shareholder cs
+        WHERE cs.id=idd;
+end;
+$$;
+
+select id from testfunction2(33);
+
+
+alter function testfunction2() owner to postgres; --automatically generated  --- postgres db can only access
+
+
+
+---PROCUEDURES---
+CREATE OR REPLACE PROCEDURE DELETE_CUSTOMERS (id integer)
+    LANGUAGE plpgsql
+AS 
+$$
+BEGIN
+    delete from customers c where c.id = id;
+END;
+$$;
+
+alter procedure delete_customers(integer) owner to postgres; --automatically generated  --- postgres db can only access
+
+CALL DELETE_CUSTOMERS(2);
